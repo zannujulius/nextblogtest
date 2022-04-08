ddc2d185b9f8c9de2f4500fca97fdb6a6b9d39f1
zannujulius <zannujulius14@gmail.com>
2022-04-08 21:35:07 +0100

    package

--- pages/404.js
+++ pages/404.js
@@ -4,12 +4,7 @@ import { useRouter } from "next/router";
 
 const NotFound = () => {
   const router = useRouter();
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     router.
-  push("/");
-  //   }, 3000);
-  // }, []);
+
   return (
     <div className="not-found">
       <h1>Opps..</h1>
